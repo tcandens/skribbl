0b7b451bbc08bbef253da98b3fe3d1ddfbc7b4fd
tcandens <joey.thies@gmail.com>
2015-09-03 15:38:27 -0700

    Working Test for Model AsyncFetch

--- test/client/skribble/spec/model.spec.js
+++ test/client/skribble/spec/model.spec.js
@@ -4,22 +4,22 @@ var expect = chai.expect;
 
 describe('Skribble Backbone Model', function() {
 
-  var Backbone, SkribbleCollection, SkribbleModel;
+  var Backbone, SkribbleModel;
 
   beforeEach(function(done) {
     require([
       'backbone',
-      'skribble/skribble_collection',
-      'skribble/skribble_model'
-    ], function( backbone, skribbleCollection, skribbleModel ) {
+      'skribble/model'
+    ], function( backbone, skribbleModel ) {
       Backbone = backbone;
-      SkribbleCollection = skribbleCollection;
       SkribbleModel = skribbleModel;
       done();
     });
 
-    this.mockCollection = new SkribbleCollection( mockModels );
-
+    // Mock server response for model fetch
+    this.mockFetch = JSON.stringify({
+      testAttr: 'test'
+    });
     // Setup Sinon Fake Server
     this.server = sinon.fakeServer.create();
   });
@@ -29,8 +29,14 @@ describe('Skribble Backbone Model', function() {
     done();
   });
 
-  it('should find itself among parent collection', function( done ) {
-
+  it('should fetch model and the execute callback with updated model passed through', function( done ) {
+    this.server.respondWith( this.mockFetch );
+    var testModel = new SkribbleModel({ id: 1 });
+    var callback = sinon.spy();
+    testModel.asyncFetch( callback );
+    this.server.respond();
+    sinon.assert.calledWith( callback, sinon.match({ attributes: {testAttr: 'test'} }) );
+    done();
   });
 
 });
